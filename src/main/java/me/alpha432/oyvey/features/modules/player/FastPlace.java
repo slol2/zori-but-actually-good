e669b6f4a717fd0988419424ac068c7f092f0ad8
Sjnez613 <52977333+Sjnez613@users.noreply.github.com>
2021-04-26 21:27:00 -0700

    Added XP mode and ALL mode to fast place
    because somertimes i want to fast place other items

--- src/main/java/me/alpha432/oyvey/features/modules/player/FastPlace.java
+++ src/main/java/me/alpha432/oyvey/features/modules/player/FastPlace.java
@@ -1,22 +1,36 @@
 package me.alpha432.oyvey.features.modules.player;
 
 import me.alpha432.oyvey.features.modules.Module;
+import me.alpha432.oyvey.features.setting.Setting;
 import me.alpha432.oyvey.util.InventoryUtil;
 import net.minecraft.item.ItemExpBottle;
 
 public class FastPlace extends Module {
+    private Setting<Item> Items;
+
+
     public FastPlace() {
         super("FastPlace", "Fast everything.", Module.Category.PLAYER, true, false, false);
+        this.Items = (Setting<Item>) this.register(new Setting("Items", Item.XP));
     }
 
-    @Override
     public void onUpdate() {
+
         if (FastPlace.fullNullCheck()) {
             return;
         }
-        if (InventoryUtil.holdingItem(ItemExpBottle.class)) {
-            FastPlace.mc.rightClickDelayTimer = 0;
+            if (Items.getValue().equals(Item.XP)) {
+                if (Items.getValue().equals(Item.XP) && InventoryUtil.holdingItem(ItemExpBottle.class)) {
+                    FastPlace.mc.rightClickDelayTimer = 0;
+                }
+            } else {
+                FastPlace.mc.rightClickDelayTimer = 0;
+            }
         }
-    }
+
+        private enum Item {
+            XP,
+            ALL;
+        }
 }
 
